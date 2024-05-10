7d9dfd102dac7e641f170c7dd4d3da7d1920dc83
XWG <2580424451@qq.com>
2024-05-10 08:59:24 +0800

    mihoyo

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ auto.waitFor();
 // 启动函数
 function startStudy() {
     app.launchApp("学习强国");
-    sleep(6000);
+    sleep(3000);
 }
 
 // 读文章函数
 function readArticle() {
+    // 点击文章
+    click(175, 235);
+    sleep(random(2000, 3000));
     // 循环12次
     for (var i = 0; i < 12; i++) {
-        // 点击文章
-        click(175, 235);
-        sleep(random(2000, 3000));
+
         // 点击阅读
         click(350, 560);
         sleep(random(2000, 3000));
@@ -19,7 +20,6 @@ function readArticle() {
         click(580, 140);
         // 等待60~90秒
         sleep(random(60000, 90000));
-        sleep(random(2000, 3000));
         // 返回
         back();
         sleep(2000);
@@ -31,11 +31,13 @@ function readArticle() {
 
 // 看电视函数
 function watchTV() {
+    // 点击电视台
+    click(500, 1550);
+    sleep(random(2000, 3000));
+    click(450,330);
+    sleep(random(20000,3000));
     // 循环12次
     for (var i = 0; i < 12; i++) {
-        // 点击电视台
-        click(500, 1550);
-        sleep(random(2000, 3000));
         // 点击播放
         click(350, 560);
         // 等待60~90秒
@@ -60,3 +62,14 @@ function executeTasks() {
     watchTV();
     sleep(random(2000, 3000));
 }
+
+// 执行任务
+executeTasks();
+home();
+//接着去米游社签到
+//打开米游社
+appapp.launchApp("米游社");
+//等待米游社启动
+sleep(random(4000, 5000));
+click(460, 250);
+
